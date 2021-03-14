26fb45fdae0d74f64474bc7155d57546411b34c2
Darian Martinez <darian.mtz18@gmail.com>
2021-03-14 09:38:15 -0500

    working on the navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+// FONT FONTAWESOME IMPORTS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/react-fontawesome";
+
+function App() {
+  return (
+    <Navbar />
+  );
+}
+
+export default App;
