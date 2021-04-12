beae743562495df877c2ce167f626192c193d7b3
Darian Martinez <darian.mtz18@gmail.com>
2021-04-12 08:05:08 -0500

    added some styling to the about me page

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Typed from "react-typed";
+import me from "../images/Me.png";
 
 const Aboutme = () => {
     return(
@@ -12,6 +13,15 @@ const Aboutme = () => {
                     backSpeed={60}
                     loop
                 />
+                <div className="card-body">
+                    {/* <div className="shadow-lg p-3 mb-5 rounded"> */}
+                        <h5>About Me</h5>
+                        <p>With supporting text below as a natural lead-in to additional content.</p>
+                    {/* </div> */}
+                </div>
+                {/* <div className="img">
+                    <image name={""} src={""} />
+                </div> */}
             </div>
         </div>
     )
