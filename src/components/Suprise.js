d77f7e3d06c970b53ca2faa285cdcb4b4784fe93
Darian Martinez <darian.mtz18@gmail.com>
2021-05-14 08:47:14 -0500

    just some finishing touches on the contact page

--- src/components/Suprise.js
+++ src/components/Suprise.js
@@ -1,13 +1,23 @@
 import React from "react";
 import surprise from "../images/suprise.png";
 import "../components/suprise.css";
+import Typed from "react-typed";
 
 
 const Surprise = () => {
     return(
         <div className="about-wraper">
+         <div className="main-info">
             <div className="img-one">
                 <img name={surprise} src={surprise} />
+                <Typed 
+                    className="typed-text"
+                    strings={["Darian Martinez Has Joined The Party! = )"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    
+                />
+                </div>
             </div>
         </div>
     )
