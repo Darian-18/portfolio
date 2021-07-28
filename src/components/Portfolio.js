85c4b29c556278fc63aeed5c1751d4db8a48b10b
Darian  Martinez <darian.mtz18@gmail.com>
2021-07-28 09:29:57 -0500

    added more projects and increased font size in about me section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,12 +11,15 @@ const Portfolio = () => {
         <div className="header-wraper">
             <div className="contact">
                 <div className="main-info">
-                    <a className="btn-main-offer" id="button" href="https://magic-ball-peds5byfu-darian-18.vercel.app">8-Ball</a>
+                    <a className="btn-main-offer" id="button" href="https://magic-ball-navy.vercel.app/">8-Ball</a>
                     <a className="btn-main-offer" id="button" href="https://the-living-haiku.web.app/">Haiku</a>
-                    <a className="btn-main-offer" id="button" href="https://breakout-a13veiybw-darian-18.vercel.app">Breakout</a>
+                    <a className="btn-main-offer" id="button" href="https://breakout-ivory.vercel.app/">Breakout</a>
+                    <a className="btn-main-offer" id="button" href="https://hex-chi.vercel.app/">Hex Color</a>
+                    <a className="btn-main-offer" id="button" href="https://calculator-alpha-olive.vercel.app/">Calculator</a>
                 </div>
             </div>
-        {/* <img className="ball" name={ball} src={ball} width="150px" height="150px"/>
+        {/* 
+        <img className="ball" name={ball} src={ball} width="150px" height="150px" border-radius="20px"/>
         <img className="breakout" name={breakout} src={breakout} width="150px" height="150px" />
         <img className="game" name={game} src={game} width="150px" height="150px" />
         <img className="haiku" name={haiku} src={haiku} width="150px" height="150px" /> */}
